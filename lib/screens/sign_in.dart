1a3820613edd47c78c0d4adc2841b2c272f31ac6
mohamed abderlaziz <57459150+lovren43@users.noreply.github.com>
2022-11-03 14:35:57 +0200

    phone number edit and login buttom

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:semsark/components/EmailField.dart';
+import 'package:semsark/components/InputField.dart';
+import 'package:semsark/components/button.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
@@ -14,11 +15,44 @@ class SignInPage extends StatelessWidget {
           const Text(
             'PLEASE ENTER YOUR ACCOUN HERE',
             style: TextStyle(
-              color:Color(0xFF8189B0),
+              color: Color(0xFF8189B0),
               fontSize: 15,
             ),
           ),
-          EmailField(),
+          const SizedBox(
+            height: 20,
+          ),
+          InputField('Phone number', const Icon(Icons.phone_android_outlined)),
+          const SizedBox(
+            height: 10,
+          ),
+          InputField('Password', const Icon(Icons.lock_outlined)),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            'Forget passowrd?',
+            style: TextStyle(
+              color: Color(0xFFFF6A6A),
+              decoration: TextDecoration.underline,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          CustomButon(text: 'Login'),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("Dont't have an account?"),
+              Text('      Sign Up',style: TextStyle(
+                color: Color(0xFF45A6DD)
+              ),)
+            ],
+          ),
         ],
       ),
     );
