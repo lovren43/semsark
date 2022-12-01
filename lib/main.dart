15d53473dbaf1b39113061b0db4b99038d250921
islamabououf <eng.islam.abououf@gmail.com>
2022-12-01 04:19:50 +0200

    Adding Map to home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:semsark/screens/islam_screens/HomeScreen.dart';
 import 'package:semsark/screens/joo_screens/SplashScreen.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class Semsark extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen()
+      home: HomeScreen()
     );
   }
 }
--- lib/screens/islam_screens/HomeScreen.dart
+++ lib/screens/islam_screens/HomeScreen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:semsark/components/button.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+  Widget build(BuildContext context) => Scaffold(
+    body: Stack(
+      children: [
+        GoogleMap(
+            initialCameraPosition:CameraPosition(target: LatLng(30.0276 , 31.2101), zoom: 15),
+        ),
+        Container(
+
+          alignment: Alignment.bottomRight,
+          padding: const EdgeInsets.symmetric(vertical: 5.0 , horizontal: 5.0),
+          child: Column(
+            children: [
+              FloatingActionButton(onPressed:  _buttonPressed,
+                backgroundColor: Colors.greenAccent,
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
+
+  void _buttonPressed(){
+
   }
 }
