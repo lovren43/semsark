5f18e8a3e7160be97c505d88c3a38c058a3248b8
Youssef Soultan <youssefsoultan5@gmail.com>
2022-12-10 19:22:00 +0200

    splashScreen

--- lib/screens/joo_screens/SplashScreen.dart
+++ lib/screens/joo_screens/SplashScreen.dart
@@ -5,6 +5,31 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body:       Container(
+          width: double.infinity,
+          height: double.infinity,
+          color: Color.fromRGBO(69, 166, 221, 1),
+          child: Column(
+              children: [
+                Expanded(
+                    child: Center(
+                          child: Image.asset('assets/images/Animation.gif'),
+                    )
+                ),
+                const Text('WITH YOU',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35.0,
+                        fontFamily: 'Hacen',
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 20,
+                    )
+                ),
+                const SizedBox(height: 100),
+              ]
+          )
+      )
+    );
   }
 }
