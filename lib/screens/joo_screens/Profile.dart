65537e607f25dd24de2994fc1685030ee2d0e1bf
Youssef Soultan <youssefsoultan5@gmail.com>
2022-12-11 05:53:41 +0200

    add profile

---
+++ lib/screens/joo_screens/Profile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class Profile extends StatefulWidget{
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
