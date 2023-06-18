f881c4e8d25d74c6700ec3ef74fbfd4ad2f85bef
islamabououf <eng.islam.abououf@gmail.com>
2023-06-18 21:19:09 +0300

    starting refactor1

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,5 +1,6 @@
 library my_prj.globals;
 
+String tokken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJlc2xhbUBlc2xhbS5jbyIsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJleHAiOjE2ODAyNjE4MzQsImlhdCI6MTY3ODQ2MTgzNH0.4JdVGFWIMXMsXKC6EMHbCKwbgVt9EmCGo88-yMqPpd2vSfH4lUo_ToU_efPF65uFSd8c_MkXdQ1SJNH4b-ByIg";
 var headers = {
   'Access-Control-Allow-Headers':
   "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Authorization",
