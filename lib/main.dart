5f548a5bcf1453c40cb0bf7fb2519ce1c4ee2ef7
mohamed abderlaziz <57459150+lovren43@users.noreply.github.com>
2023-06-19 20:08:35 +0300

    Merge branch 'app_with_provider' of C:\Users\Lovren\Documents\semsark with conflicts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+<<<<<<< Updated upstream
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:semsark/adapters/local_ad_adapter.dart';
 import 'package:semsark/screens/splash_screen.dart';
+=======
+import 'package:semsark/screens/joo_screens/Profile.dart';
+import 'package:semsark/screens/joo_screens/SplashScreen.dart';
+import 'package:semsark/screens/lovren_screens/sign_in.dart';
+>>>>>>> Stashed changes
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,9 +23,9 @@ class Semsark extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: SplashScreen()
+        home: LoginScreen()
     );
   }
 }
