7f5719aaa1628cdf1547ef1d00c3cb4bf1d000dc
islamabououf <eng.islam.abououf@gmail.com>
2023-06-23 14:32:05 +0300

    add filter_provider.dart and filter_screen.dart

--- lib/utils/end_points.dart
+++ lib/utils/end_points.dart
@@ -1,9 +1,11 @@
-const String baseURL = "http://10.0.2.2:8080";
-//const String baseURL = "http://localhost:8080";
-const String CREATE_AD = "createAd";
-const String GET_USER = "getUser";
+//const String baseURL = "https://10.0.2.2:8000";
+const String baseURL = "http://localhost:8080";
+const String GET_USER = "$baseURL/user/getUser";
 const String VERFIY_EMAIL = "$baseURL/insecure/userDetails/checkEmail";
 const String LOGIN = "$baseURL/insecure/authenticate";
 const String SEND_OTP = "$baseURL/email/sendOtp";
 const String VERIFY_OTP = "$baseURL/email/verifyEmail";
 const String CREATE_USER = "$baseURL/insecure/userDetails";
+const String GET_ALL_ADS = "$baseURL/common/getAllAds";
+const String FILTER = "$baseURL/user/filter";
+const String CREATE_AD = "$baseURL/user/createAd";
